0df7fd25d2427b244f69f309d10f1b3bccdec884
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-15 22:52:54 +0530

    chore: add solution to question 3

---
+++ question3.js
@@ -0,0 +1,36 @@
+// using normal function
+function changeCapitalization(input) {
+    let size = input.length
+    let output = ''
+
+    for(let i = 0; i < size; i++) {
+        let alphabet = input[i]
+        if(alphabet.toUpperCase() === alphabet) {
+            alphabet = alphabet.toLowerCase();
+        }
+        else if(alphabet.toLowerCase() === alphabet) {
+            alphabet = alphabet.toUpperCase();
+        }
+        output = output + alphabet
+    }
+
+    return output
+}
+
+// using arrow function
+const changeCapitalizationArrow = (input) => {
+    let size = input.length
+    let output = ''
+
+    for(let i = 0; i < size; i++) {
+        let alphabet = input[i]
+        if(alphabet.toUpperCase() === alphabet) {
+            output = output + alphabet.toLowerCase();
+        }
+        else if(alphabet.toLowerCase() === alphabet) {
+            output = output + alphabet.toUpperCase();
+        }
+    }
+
+    return output
+}
