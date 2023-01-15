66f0390193ea5392cfb7e85825a1d9f4af6e6166
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 01:15:14 +0530

    chore: add tests for question 5

--- question5.js
+++ question5.js
@@ -9,3 +9,5 @@ const isLeapYearArrow = (year) => {
     let result = ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) ? true : false
     return result
 }
+
+module.exports = {isLeapYear, isLeapYearArrow}
