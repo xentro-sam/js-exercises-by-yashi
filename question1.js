579d8c08e9c17e4959cd5634bc7c4998953253e2
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-15 21:32:03 +0530

    chore: add solution to question 1

---
+++ question1.js
@@ -0,0 +1,33 @@
+// using normal function
+function getLargestEvenNumber(numbers) {
+    let largestEven = Number.MIN_VALUE
+
+    numbers.forEach(function(element) {
+        if(element % 2 === 0 && element > largestEven) {
+            largestEven = element
+        }
+    });
+
+    if(largestEven === Number.MIN_VALUE) {
+        largestEven = -1
+    }
+
+    return largestEven
+}
+
+// using arrow function
+const getLargestEvenArrow = (numbers) => {
+    let largestEven = Number.MIN_VALUE
+
+    numbers.forEach(function(element) {
+        if(element % 2 === 0 && element > largestEven) {
+            largestEven = element
+        }
+    });
+
+    if(largestEven === Number.MIN_VALUE) {
+        largestEven = -1
+    }
+
+    return largestEven
+}
