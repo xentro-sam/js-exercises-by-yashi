4f59192e77f84d94695df061a7ac8baf49bfda00
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-15 23:03:08 +0530

    chore: add solution to question 4

---
+++ question4.js
@@ -0,0 +1,27 @@
+// using normal function
+function getLongestString(input) {
+    let longestString = ''
+    let size = input.length
+
+    for(let i = 0; i < size; i++) {
+        if(input[i].length > longestString.length) {
+            longestString = input[i]
+        }
+    }
+
+    return longestString
+}
+
+// using arrow function
+const getLongestStringArrow = (input) => {
+    let longestString = ''
+    let size = input.length
+
+    for(let i = 0; i < size; i++) {
+        if(input[i].length > longestString.length) {
+            longestString = input[i]
+        }
+    }
+
+    return longestString
+}
