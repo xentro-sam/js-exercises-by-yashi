15032400e2e406932ed919ed2e46596df33bbb0f
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-15 23:16:21 +0530

    chore: add solution to question 5

---
+++ question5.js
@@ -0,0 +1,11 @@
+// using normal function
+function isLeapYear(year) {
+    let result = ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) ? true : false
+    return result
+}
+
+// using arrow function
+const isLeapYearArrow = (year) => {
+    let result = ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) ? true : false
+    return result
+}
