5ff78f029dc945cd197015dc81811d17e5bc0f9e
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 01:40:19 +0530

    chore: add tests for question 8

--- question8.js
+++ question8.js
@@ -27,3 +27,5 @@ const countVowelsArrow = (input) => {
 
     return count
 }
+
+module.exports = {countVowels, countVowelsArrow}
---
+++ question8.test.js
@@ -0,0 +1,18 @@
+const {countVowels, countVowelsArrow} = require('./question8')
+
+test('"codeacademy" should return "5"', () => {
+    expect(countVowels("codeacademy")).toBe(5)
+})
+
+test('"shivam" should return "2"', () => {
+    expect(countVowels("shivam")).toBe(2)
+})
+
+
+test('"codeacademy" should return "5"', () => {
+    expect(countVowelsArrow("codeacademy")).toBe(5)
+})
+
+test('"shivam" should return "2"', () => {
+    expect(countVowelsArrow("shivam")).toBe(2)
+})
