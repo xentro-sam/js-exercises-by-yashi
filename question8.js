267c60d3c27430541613475cf2aac1528eb3582f
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-15 23:38:00 +0530

    chore: add solution to question 8

---
+++ question8.js
@@ -0,0 +1,29 @@
+// using normal function
+function countVowels(input) {
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    let size = input.length
+    let count = 0
+
+    for(let i = 0; i < size; i++) {
+        if(vowels.includes(input[i])) {
+            count++
+        }
+    }
+
+    return count
+}
+
+// using arrow function
+const countVowelsArrow = (input) => {
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    let size = input.length
+    let count = 0
+
+    for(let i = 0; i < size; i++) {
+        if(vowels.includes(input[i])) {
+            count++
+        }
+    }
+
+    return count
+}
