fb6feb7cb0656b7b28d505a67a7f0eb759f3981b
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 00:00:05 +0530

    chore: add solution to question 9

---
+++ question9.js
@@ -0,0 +1,37 @@
+// using normal function
+function replaceCharacterWithNext(input) {
+    let size = input.length
+    let result = ''
+
+    for(let i = 0; i < size; i++) {
+        let code = input.charCodeAt(i) + 1;
+        if(code === 91) {
+            code = 65
+        }
+        else if(code === 123) {
+            code = 97
+        }
+        result = result + String.fromCharCode(code)
+    }
+
+    return result
+}
+
+// using arrow function
+const replaceCharacterWithNextArrow = (input) => {
+    let size = input.length
+    let result = ''
+
+    for(let i = 0; i < size; i++) {
+        let code = input.charCodeAt(i) + 1;
+        if(code === 91) {
+            code = 65
+        }
+        else if(code === 123) {
+            code = 97
+        }
+        result = result + String.fromCharCode(code)
+    }
+
+    return result
+}
