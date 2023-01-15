060b288dc7e7ace9415ab1b81a5eca8b9ee13064
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-15 21:52:21 +0530

    chore: add solution to question 2

---
+++ question2.js
@@ -0,0 +1,33 @@
+// using normal function
+function checkSameDigits(num) {
+    let isSame = true
+    let toCheck = num % 10;
+
+    while(num > 0 && isSame === true) {
+        let digit = num % 10
+        if(digit !== toCheck) {
+            isSame = false
+        }
+        num /= 10;
+        num = Math.trunc(num)
+    }
+
+    return isSame
+}
+
+// using arrow function
+const checkSameDigitsArrow = (num) => {
+    let isSame = true
+    let toCheck = num % 10;
+
+    while(num > 0 && isSame === true) {
+        let digit = num % 10
+        if(digit !== toCheck) {
+            isSame = false
+        }
+        num /= 10;
+        num = Math.trunc(num)
+    }
+
+    return isSame
+}
