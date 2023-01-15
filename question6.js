65f6aa8b049890ac07f4120368ad867b02e56919
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-15 23:23:33 +0530

    chore: add solution to question 6

---
+++ question6.js
@@ -0,0 +1,11 @@
+// using normal function
+function hasScript(input) {
+    let result = input.endsWith('Script')
+    return result
+}
+
+// using arrow function
+const hasScriptArrow = (input) => {
+    let result = input.endsWith('Script')
+    return result
+}
