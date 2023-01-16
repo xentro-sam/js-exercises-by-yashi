cdee11212e38b91c628869968981acafc11f8237
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 15:00:41 +0530

    chore: apply linting to solution of question 3

--- question3.js
+++ question3.js
@@ -1,38 +1,38 @@
 // using normal function
 function changeCapitalization(input) {
-    let size = input.length
-    let output = ''
+  let size = input.length;
+  let output = '';
 
-    for(let i = 0; i < size; i++) {
-        let alphabet = input[i]
-        if(alphabet.toUpperCase() === alphabet) {
-            alphabet = alphabet.toLowerCase();
-        }
-        else if(alphabet.toLowerCase() === alphabet) {
-            alphabet = alphabet.toUpperCase();
-        }
-        output = output + alphabet
+  for(let i = 0; i < size; i++) {
+    let alphabet = input[i];
+    if(alphabet.toUpperCase() === alphabet) {
+      alphabet = alphabet.toLowerCase();
     }
+    else if(alphabet.toLowerCase() === alphabet) {
+      alphabet = alphabet.toUpperCase();
+    }
+    output = output + alphabet;
+  }
 
-    return output
+  return output;
 }
 
 // using arrow function
 const changeCapitalizationArrow = (input) => {
-    let size = input.length
-    let output = ''
+  let size = input.length;
+  let output = '';
 
-    for(let i = 0; i < size; i++) {
-        let alphabet = input[i]
-        if(alphabet.toUpperCase() === alphabet) {
-            output = output + alphabet.toLowerCase();
-        }
-        else if(alphabet.toLowerCase() === alphabet) {
-            output = output + alphabet.toUpperCase();
-        }
+  for(let i = 0; i < size; i++) {
+    let alphabet = input[i];
+    if(alphabet.toUpperCase() === alphabet) {
+      output = output + alphabet.toLowerCase();
     }
+    else if(alphabet.toLowerCase() === alphabet) {
+      output = output + alphabet.toUpperCase();
+    }
+  }
 
-    return output
-}
+  return output;
+};
 
-module.exports = {changeCapitalization, changeCapitalizationArrow}
+module.exports = {changeCapitalization, changeCapitalizationArrow};
