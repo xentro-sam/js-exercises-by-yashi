64c7dfcc8d4fa9afb5b155f0ffa35059e08b6746
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 15:01:02 +0530

    chore: apply linting to solution of question 1

--- question1.js
+++ question1.js
@@ -1,35 +1,35 @@
 // using normal function
 function getLargestEvenNumber(numbers) {
-    let largestEven = Number.MIN_VALUE
+  let largestEven = Number.MIN_VALUE;
 
-    numbers.forEach(function(element) {
-        if(element % 2 === 0 && element > largestEven) {
-            largestEven = element
-        }
-    });
-
-    if(largestEven === Number.MIN_VALUE) {
-        largestEven = -1
+  numbers.forEach(function(element) {
+    if(element % 2 === 0 && element > largestEven) {
+      largestEven = element;
     }
+  });
 
-    return largestEven
+  if(largestEven === Number.MIN_VALUE) {
+    largestEven = -1;
+  }
+
+  return largestEven;
 }
 
 // using arrow function
 const getLargestEvenArrow = (numbers) => {
-    let largestEven = Number.MIN_VALUE
+  let largestEven = Number.MIN_VALUE;
 
-    numbers.forEach(function(element) {
-        if(element % 2 === 0 && element > largestEven) {
-            largestEven = element
-        }
-    });
-
-    if(largestEven === Number.MIN_VALUE) {
-        largestEven = -1
+  numbers.forEach(function(element) {
+    if(element % 2 === 0 && element > largestEven) {
+      largestEven = element;
     }
+  });
 
-    return largestEven
-}
+  if(largestEven === Number.MIN_VALUE) {
+    largestEven = -1;
+  }
 
-module.exports = {getLargestEvenNumber, getLargestEvenArrow}
+  return largestEven;
+};
+
+module.exports = {getLargestEvenNumber, getLargestEvenArrow};
