8db28cd641b0955389eda18bddb57a55d613d626
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 15:00:33 +0530

    chore: apply linting to solution of question 4

--- question4.js
+++ question4.js
@@ -1,35 +1,35 @@
 // using normal function
 function getLongestString(input) {
-    let longestString = ''
-    let size = input.length
+  let longestString = '';
+  let size = input.length;
 
-    for(let i = 0; i < size; i++) {
-        if(input[i].length > longestString.length) {
-            longestString = input[i]
-        }
-        else if(input[i].length === longestString.length) {
-            longestString = input[i] < longestString ? input[i] : longestString;
-        }
+  for(let i = 0; i < size; i++) {
+    if(input[i].length > longestString.length) {
+      longestString = input[i];
     }
+    else if(input[i].length === longestString.length) {
+      longestString = input[i] < longestString ? input[i] : longestString;
+    }
+  }
 
-    return longestString
+  return longestString;
 }
 
 // using arrow function
 const getLongestStringArrow = (input) => {
-    let longestString = ''
-    let size = input.length
+  let longestString = '';
+  let size = input.length;
 
-    for(let i = 0; i < size; i++) {
-        if(input[i].length > longestString.length) {
-            longestString = input[i]
-        }
-        else if(input[i].length === longestString.length) {
-            longestString = input[i] < longestString ? input[i] : longestString;
-        }
+  for(let i = 0; i < size; i++) {
+    if(input[i].length > longestString.length) {
+      longestString = input[i];
     }
+    else if(input[i].length === longestString.length) {
+      longestString = input[i] < longestString ? input[i] : longestString;
+    }
+  }
 
-    return longestString
-}
+  return longestString;
+};
 
-module.exports = {getLongestString, getLongestStringArrow}
+module.exports = {getLongestString, getLongestStringArrow};
