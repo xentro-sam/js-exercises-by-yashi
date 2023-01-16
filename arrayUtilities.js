02e02258ac0b6a630c24ac0098cee8c795dd5a62
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 16:18:36 +0530

    chore: write an array utility and test it

---
+++ arrayUtilities.js
@@ -0,0 +1,16 @@
+const tripleElementsAndFilterEven = (numbers) => {
+    if(!Array.isArray(numbers)) {
+        throw new TypeError();
+    }
+
+    let result = numbers.map(item => item * 3);
+    result = result.filter((item) => {
+        if(item % 2 === 0) {
+            return item;
+        }
+    });
+
+    return result;
+}
+
+module.exports = { tripleElementsAndFilterEven };
