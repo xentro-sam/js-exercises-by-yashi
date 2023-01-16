fcefaf2dcaeba4fbf2660c3aa71cf218a003e064
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 14:59:23 +0530

    chore: apply linting to solution of question 7

--- question7.js
+++ question7.js
@@ -1,13 +1,13 @@
 // using normal function
 function joinExceptFirst(input) {
-    let result = input[0].substring(1) + input[1].substring(1)
-    return result
+  let result = input[0].substring(1) + input[1].substring(1);
+  return result;
 }
 
 // using arrow function
 const joinExceptFirstArrow = (input) => {
-    let result = input[0].substring(1) + input[1].substring(1)
-    return result
-}
+  let result = input[0].substring(1) + input[1].substring(1);
+  return result;
+};
 
-module.exports = {joinExceptFirst, joinExceptFirstArrow}
+module.exports = {joinExceptFirst, joinExceptFirstArrow};
