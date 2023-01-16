29167c27b574ab431e0ec4da85418b172671aefc
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 14:57:23 +0530

    chore: apply linting to solution of question 8

--- question8.js
+++ question8.js
@@ -1,31 +1,31 @@
 // using normal function
 function countVowels(input) {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    let size = input.length
-    let count = 0
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  let size = input.length;
+  let count = 0;
 
-    for(let i = 0; i < size; i++) {
-        if(vowels.includes(input[i])) {
-            count++
-        }
+  for(let i = 0; i < size; i++) {
+    if(vowels.includes(input[i])) {
+      count++;
     }
+  }
 
-    return count
+  return count;
 }
 
 // using arrow function
 const countVowelsArrow = (input) => {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    let size = input.length
-    let count = 0
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  let size = input.length;
+  let count = 0;
 
-    for(let i = 0; i < size; i++) {
-        if(vowels.includes(input[i])) {
-            count++
-        }
+  for(let i = 0; i < size; i++) {
+    if(vowels.includes(input[i])) {
+      count++;
     }
+  }
 
-    return count
-}
+  return count;
+};
 
-module.exports = {countVowels, countVowelsArrow}
+module.exports = {countVowels, countVowelsArrow};
