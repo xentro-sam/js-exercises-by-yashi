2faa2dff808ddc94fd00992122dfc9801d2d3022
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 17:59:47 +0530

    chore: apply linting to array utilities solution and test file

--- arrayUtilities.js
+++ arrayUtilities.js
@@ -1,16 +1,16 @@
 const tripleElementsAndFilterEven = (numbers) => {
-    if(!Array.isArray(numbers)) {
-        throw new TypeError();
+  if(!Array.isArray(numbers)) {
+    throw new TypeError();
+  }
+
+  let result = numbers.map(item => item * 3);
+  result = result.filter((item) => {
+    if(item % 2 === 0) {
+      return item;
     }
+  });
 
-    let result = numbers.map(item => item * 3);
-    result = result.filter((item) => {
-        if(item % 2 === 0) {
-            return item;
-        }
-    });
-
-    return result;
-}
+  return result;
+};
 
 module.exports = { tripleElementsAndFilterEven };
--- arrayUtilities.test.js
+++ arrayUtilities.test.js
@@ -1,12 +1,12 @@
-const { tripleElementsAndFilterEven } = require('./arrayUtilities')
+const { tripleElementsAndFilterEven } = require('./arrayUtilities');
 
-describe("Array Utilities", () => {
-    describe("Double Array Elements", () => {
-        it("should return [6] when passed [1, 2, 3]", () => {
-            expect(tripleElementsAndFilterEven([1, 2, 3])).toEqual([6])
-        });
-        it("throws an error if given a non-array input", () => {
-            expect(() => tripleElementsAndFilterEven("shivam")).toThrow(TypeError);
-        })
-    })
-})
+describe('Array Utilities', () => {
+  describe('Double Array Elements', () => {
+    it('should return [6] when passed [1, 2, 3]', () => {
+      expect(tripleElementsAndFilterEven([1, 2, 3])).toEqual([6]);
+    });
+    it('throws an error if given a non-array input', () => {
+      expect(() => tripleElementsAndFilterEven('shivam')).toThrow(TypeError);
+    });
+  });
+});
