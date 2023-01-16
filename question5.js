f3f74adbec135d1e0444d300f72f1cfa1ccc6fcb
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-16 15:00:26 +0530

    chore: apply linting to solution of question 5

--- question5.js
+++ question5.js
@@ -1,13 +1,13 @@
 // using normal function
 function isLeapYear(year) {
-    let result = ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) ? true : false
-    return result
+  let result = ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) ? true : false;
+  return result;
 }
 
 // using arrow function
 const isLeapYearArrow = (year) => {
-    let result = ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) ? true : false
-    return result
-}
+  let result = ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) ? true : false;
+  return result;
+};
 
-module.exports = {isLeapYear, isLeapYearArrow}
+module.exports = {isLeapYear, isLeapYearArrow};
